7b63722f8e5e2e9e8a76f6b4c9bd41be77641f23
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-02-25 19:27:59 +0100

    project: user update function and route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,4 +30,21 @@ class UserController extends Controller
     $saved = $this->user->create($request->all());
     return response()->json(["user" => $saved], 201);
   }
+
+  public function update(Request $request, $id)
+  {
+    $new_user = $this->user->find($id);
+    if($new_user == null)
+      return response()->json(["message" => "User not found"], 404);
+    
+    if($request->name != null)
+      $new_user->name = $request->name;
+    if($request->email != null)
+      $new_user->email = $request->email;
+    if($request->password != null)
+      $new_user->password = $request->password;
+    
+    $new_user->update();
+    return response()->json(["user" => $new_user], 200);
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@
  */
 $router->post("/user", "UserController@store");
 $router->get("/user/{id}", "UserController@index");
+$router->put("/user/{id}", "UserController@update");
