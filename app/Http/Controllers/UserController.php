5ce8952638c140b2e0546e2f8288ff9917a77672
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-02-25 20:34:22 +0100

    style: increasing code legibility

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,24 +31,30 @@ class UserController extends Controller
       $this->user->rules,
       $this->user->messages
     );
+    
     $saved = $this->user->create($request->all());
+
     return response()->json(["user" => $saved], 201);
   }
 
   public function update(Request $request, $id)
   {
     $new_user = $this->user->find($id);
+
     if($new_user == null)
       return response()->json(["message" => "User not found"], 404);
     
     if($request->name != null)
       $new_user->name = $request->name;
+    
     if($request->email != null)
       $new_user->email = $request->email;
-    if($request->password != null)
+    
+      if($request->password != null)
       $new_user->password = $request->password;
     
     $new_user->update();
+
     return response()->json(["user" => $new_user], 200);
   }
 
